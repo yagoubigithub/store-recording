0428b4b56bba33a9893c0a573ea24e6b08889377
yagoubigithub <yagoubi.aek.2@gmail.com>
2019-06-11 13:50:51 +0100

    styling navabar part 1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react'
-
+import logo from '../logo.svg';
+import {Link} from 'react-router-dom';
 export default class Navbar extends Component {
     render() {
         return (
-            <div>
-            <h3>Hello from Navbar</h3>
-                
-            </div>
+           <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+
+           <Link to="/">
+               <img src={logo} alt={"store"}  className="navbar-brand"/>
+           </Link>
+           <ul className="navbar-nav align-items-center">
+               <li className="nav-item ml-5">
+                   <Link to="/" className="nav-link">
+                   Product
+                   </Link>
+               </li>
+           </ul>
+           <Link to="/cart" className="ml-auto">
+
+               <button>
+                   <i className="fas fa-cart-plus"></i>
+                   My cart
+               </button>
+           </Link>
+
+
+           </nav>
         )
     }
 }
