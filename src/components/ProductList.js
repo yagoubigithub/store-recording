cc00ccd92797d700e1692b8ecf694191ba92ab40
yagoubigithub <yagoubi.aek.2@gmail.com>
2019-06-11 14:48:54 +0100

    Title component

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react'
 import Product from './Product';
+import Title from './Title';
 
 export default class ProductList extends Component {
+    state= {
+        products : [
+
+        ]
+    }
     render() {
         return (
-            <div>
-           
-                <Product />
-            </div>
+          <React.Fragment>
+              <div className="py-5">
+                  <div className="container">
+                  
+                      <Title name="our" title="product"/>
+                      <div className="row">  </div>
+
+                  </div>
+              </div>
+          </React.Fragment>
         )
     }
 }
