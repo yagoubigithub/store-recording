82436f4beb701d9539ff722ce1c80ecb2c67ab71
yagoubigithub <yagoubi.aek.2@gmail.com>
2019-06-19 16:34:13 +0100

    EmptyCart component

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Title from '../Title';
 
 import CartColums from './CartColums';
+import EmptyCart from './EmptyCart';
 export default class Cart extends Component {
     render() {
         return (
