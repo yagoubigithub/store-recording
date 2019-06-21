6fb2d1ddbdcd8c7380b15b062d8cb0939f09a1b9
yagoubigithub <yagoubi.aek.2@gmail.com>
2019-06-21 15:01:22 +0100

    condition when the cart is empty

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,18 +1,31 @@
-import React, { Component } from 'react'
-import Title from '../Title';
+import React, { Component } from "react";
+import Title from "../Title";
 
-import CartColums from './CartColums';
-import EmptyCart from './EmptyCart';
+import CartColums from "./CartColums";
+import EmptyCart from "./EmptyCart";
+import { ProductConsumer } from "../../context";
 export default class Cart extends Component {
-    render() {
-        return (
-            <section>
-            
-            <Title name="your" title="cart" />
+  render() {
+    return (
+      <section>
+        <ProductConsumer>
+          {value => {
+            const { cart } = value;
+            if (cart.length > 0) {
+              return (
+                <React.Fragment>
+                  <Title name="your" title="cart" />
+                  <CartColums />
+                </React.Fragment>
+              );
+            }else{
+                return( <EmptyCart />);
+            }
+          }}
+        </ProductConsumer>
 
-            <CartColums />
-                
-            </section>
-        )
-    }
+       
+      </section>
+    );
+  }
 }
--- src/context.js
+++ src/context.js
@@ -7,7 +7,7 @@ export default class ProductProvider extends Component {
     state= {
         products : [],
         detailProduct :{},
-        cart : [],
+        cart : storeProducts,
         modalOpen : false,
         modalProduct : detailProduct,
         cartSubTotal : 0,
