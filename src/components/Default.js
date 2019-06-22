bc72ae7e5bc891e045e495cffeebdf15767b3b51
yagoubigithub <yagoubi.aek.2@gmail.com>
2019-06-22 15:55:26 +0100

    Revert "page not found 404 Default.js"
    This reverts commit d5264be9629b0c0de5e76e8e4aa3d916f979fbee.

--- src/components/Default.js
+++ src/components/Default.js
@@ -2,21 +2,9 @@ import React, { Component } from 'react'
 
 export default class Default extends Component {
     render() {
-        console.log(this.props);
         return (
-            <div className="container">
-            <div className="row">
-            <div className="col-10 mx-auto text-center text-title text-uppercase pt-5">
-                <h1 className=" display-2">404</h1>
-                <h1>Error</h1>
-                <h2>Page not found</h2>
-                <h3>the request URL <span className="text-danger">{this.props.location.pathname}</span>
-                 {" "} was not found
-                </h3>
-            </div>
-
-            </div>
-               
+            <div>
+                <h3>Page not Found</h3>
             </div>
         )
     }
